c97854429d5b9d2225225d4fad105ddce76d324b
Ahmed-Brahim1 <ahmed.brahim@esprit.tn>
2025-04-07 02:13:35 +0100

    ..

--- app/page.js
+++ app/page.js
@@ -1,14 +1,19 @@
-"use client";
 import dynamic from 'next/dynamic'; // Import dynamic for client-side rendering
 import AboutSection from "./components/homepage/about";
+import HeroSection from "./components/homepage/hero-section";
+import Experience from "./components/homepage/experience";
+import Skills from "./components/homepage/skills";
+import Projects from "./components/homepage/projects";
+import Education from "./components/homepage/education";
+import ContactSection from "./components/homepage/contact";
+
+
+
+
+
+
 
 // Dynamically import the client-side components
-const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
-const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
-const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
-const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
-const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
-const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
 
 
 
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "AHMED BRAHIM",
-  profile: '/profile.jpg',
+  profile: '/Profile.jpg',
   designation: "Software Developer",
   description: "My name is AHMED BRAHIM. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on Java and I love to do most of the things using Java. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'contact.ahmedibrahim1@gmail.com',
