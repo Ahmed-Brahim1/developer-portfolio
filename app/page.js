292e95409b757e4054c1f2b197e72af7538f6c8f
Ahmed-Brahim1 <ahmed.brahim@esprit.tn>
2025-04-07 01:09:01 +0100

    ..

--- app/page.js
+++ app/page.js
@@ -1,6 +1,3 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
@@ -10,47 +7,32 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-export default function Home() {
-  const [blogs, setBlogs] = useState([]);
-  const [loading, setLoading] = useState(true);
+async function getData() {
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
 
-  // Fetch data on the client side using useEffect
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
-
-        if (!res.ok) {
-          throw new Error('Failed to fetch data');
-        }
-
-        const data = await res.json();
-        const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-        setBlogs(filtered);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getData();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>; // You can show a loader or skeleton here
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
   }
 
+  const data = await res.json();
+
+  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+
+  return filtered;
+};
+
+export default async function Home() {
+  const blogs = await getData();
+
   return (
-    <div>
+    <div suppressHydrationWarning >
       <HeroSection />
       <AboutSection />
       <Experience />
       <Skills />
-      <Projects blogs={blogs} /> {/* Pass blogs to your Projects component */}
+      <Projects />
       <Education />
       <ContactSection />
     </div>
-  );
-}
+  )
+};
